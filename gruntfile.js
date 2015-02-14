0deb82d6bcdcda8f93efecaf1693086fa6321dd6
Gerardo May <gerardomay@live.com>
2015-02-14 08:14:20 -0500

    Agregado el cajon

---
+++ gruntfile.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Gerardo on 08/02/2015.
+ */
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        stylus:{
+          commpile:{
+              options:{
+                  paths:['src/stylus/import']
+              },
+              files:{
+                  'app/css/main.min.css':'src/stylus/main.styl'
+              }
+          }
+        },
+        uglify:{
+            options:{
+                sourceMap:true,
+                sourceMapIncludesSourcces:true,
+                compress:{
+                    drop_console:false
+                }
+            },
+            main:{
+                files:{
+                    'public/js/main.min.js':['src/js/*.js']
+                }
+            }
+        },
+        browserSync:{
+            dev:{
+                bsFiles:{
+                    src:['app/css/*.css', 'app/*.html', 'app/js/main.min.js']
+                },
+                options:{
+                    watchTask:true
+                }
+            }
+        },
+        watch:{
+            stylesheets:{
+                files:'src/stylus/**/*.styl',
+                tasks: ['stylus']
+            },
+            script:{
+                files:'src/js/*.js',
+                tasks:'uglify'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default',['stylus', 'uglify', 'browserSync', 'watch']);
+
+};
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Gerardo on 14/02/2015.
+ */
